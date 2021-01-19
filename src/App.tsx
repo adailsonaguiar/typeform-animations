361770e1d400e2a55b31ac02acd6819b3cff24ec
Adailson Aguiar <adailsonaguiar@pop-os.localdomain>
2021-01-19 11:28:00 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FirstAndSecond from "./components/FirstAndSecond/Index";
+import GlobalStyles from "./styles/global";
+import * as S from "./styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <S.Main style={{ height: "568vh" }}>
+        <S.Section style={{ height: "72.7%" }}>
+          <FirstAndSecond />
+        </S.Section>
+
+        <S.Section style={{ height: "9.7%" }}>
+          <S.Sticky className="third" />
+        </S.Section>
+
+        <S.Section style={{ height: "10.1%" }}>
+          <S.Sticky className="fourth" />
+        </S.Section>
+      </S.Main>
+    </>
   );
 }
 
